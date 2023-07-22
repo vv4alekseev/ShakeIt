acbe49cd382b9d856f7979cf04e4318344fde6c7
Vladislav Alekseev <vv4alekseev@gmail.com>
2023-07-22 23:54:19 +0300

    Update apply_camera_shake.py

--- apply_camera_shake.py
+++ apply_camera_shake.py
@@ -17,43 +17,63 @@ from video_processing import (read_video,
 
 def apply_camera_shake(args) -> None:
     """
-    text
+    Apply camera shake effects to a video sequence.
+
+    Parameters:
+        args (dict): A dictionary containing various parameters and settings.
+    
+    Returns:
+        None
     """
+    # Read config file
     init_logging()
     configs = read_config(path="configs.yaml")
-    
+
+    # Read video
     video, fps = read_video(args.input_video)
     f, h_orig, w_orig, _ = video.shape
-    
+
+    # Setup device
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+    # Model initialization
     generator = init_model(device=device)
 
+    # Inpaint some frames
     inpainted_frames, idxs = inpaint_n_frames(video=video, 
                                               each_x_frame=configs["each_x_frame"], 
                                               padding=32,
                                               generator=generator,
                                               device=device)
 
-
+    # Interpolate missed frames
     video = interpolate_frames(video=video, 
                                inpainted_frames=inpainted_frames,
                                padding=configs["padding"],
                                idxs=idxs)
-    
+
+    # Generate a random waveform function along the y-axis
     y_trans = generate_translation(n_frames=f, 
                                    fps=fps, 
                                    amplitudes=configs["y_amplitudes"], 
                                    frequencies=configs["y_frequencies"])
-    
+
+    # Generate a random waveform function along the x-axis
     x_trans = generate_translation(n_frames=f, 
                                    fps=fps, 
                                    amplitudes=configs["x_amplitudes"], 
                                    frequencies=configs["x_frequencies"])
-    
+
+    # Apply the shake effect
     video = shake(video, x_trans, y_trans)
+
+    # Crop the video to the initial size
     video = crop_video(video=video, size=(h_orig, w_orig))
+
+    # Apply a mask to the video
     video = to_circular(video=video)
+
+    # Save the video
     save_video(args.output_video, video, fps)
 
 
